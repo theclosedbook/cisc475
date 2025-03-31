d020326dae90ad413e7ad640a33faff6152fe6e7
Stephen Siegel <siegel@udel.edu>
2025-03-31 16:32:13 -0400

    Making Rex an Eclipse Project.

--- Rex/src/edu/udel/cisc675/rex/Input.java
+++ Rex/src/edu/udel/cisc675/rex/Input.java
@@ -10,31 +10,34 @@ import java.io.IOException;
    field chars. */
 public class Input {
 
-    /* The name of the original file containing the exam (in) */
-    String filename;
+	/* The name of the original file containing the exam (in) */
+	String filename;
 
-    /* The contents of the file, as an array of char (out) */
-    char[] chars;
+	/* The contents of the file, as an array of char (out) */
+	char[] chars;
 
-    /* Constructs a new instance with given filename.  Sets the
-       filename field and does nothing else */
-    public Input(String filename) {
-	this.filename = filename;
-    }
-
-    /* Opens the file, reads it, and constructs chars. */
-    public void execute() throws FileNotFoundException, IOException {
-	FileReader fr = new FileReader(filename);
-	int off = 0; // number of chars read
-	chars = new char[2]; // start at 2 but double each time cap reached
-	while (true) {
-	    int nread = fr.read(chars, off, chars.length - off);
-	    if (nread < 0) break;
-	    off += nread;
-	    if (off == chars.length) // double buffer size
-		chars = Arrays.copyOf(chars, 2*chars.length);
+	/*
+	 * Constructs a new instance with given filename. Sets the filename field
+	 * and does nothing else
+	 */
+	public Input(String filename) {
+		this.filename = filename;
 	}
-	fr.close();
-	chars = Arrays.copyOf(chars, off); // trim to exact size of data
-    }
+
+	/* Opens the file, reads it, and constructs chars. */
+	public void execute() throws FileNotFoundException, IOException {
+		FileReader fr = new FileReader(filename);
+		int off = 0; // number of chars read
+		chars = new char[2]; // start at 2 but double each time cap reached
+		while (true) {
+			int nread = fr.read(chars, off, chars.length - off);
+			if (nread < 0)
+				break;
+			off += nread;
+			if (off == chars.length) // double buffer size
+				chars = Arrays.copyOf(chars, 2 * chars.length);
+		}
+		fr.close();
+		chars = Arrays.copyOf(chars, off); // trim to exact size of data
+	}
 }
